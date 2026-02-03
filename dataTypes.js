c90fd89ded84261552801a61868b8aca9ecd5a08
Hamza <hamzafarooq49ml@gmail.com>
2026-02-03 08:22:20 +0500

    loops done

--- dataTypes.js
+++ dataTypes.js
@@ -208,15 +208,15 @@ fruits.length = 0;
 
 // Two sum
 
-let twoSum = function (nums, target) {
-  let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      sum = nums[i] + nums[j];
-      if (sum === target) {
-        return [i, j];
-      }
-    }
-  }
-};
-console.log(twoSum([4, 7, 2, 15], 9));
+// let twoSum = function (nums, target) {
+//   let sum = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       sum = nums[i] + nums[j];
+//       if (sum === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+// console.log(twoSum([4, 7, 2, 15], 9));
---
+++ loops.js
@@ -0,0 +1,79 @@
+// Basic loops
+
+//  While loop
+
+// while condition is true, execute the code block
+
+// let count = 10;
+// while (count) {
+//   // we can also use truthy values in condition
+//   console.log("first");
+//   count--;
+// }
+// console.log("While loop ended");
+
+// Do...while loop
+// similar as while but the condition is run atleast once regardless of the condition
+
+// for loop
+// i laready mastered it millions of times
+
+// for (let i = 10; i; i--) {
+//   console.log(i);
+// }
+
+// we can ommit parts of for loop
+// let i = 10;
+// for (; i; ) {
+//   console.log(i);
+//   i--;
+// }
+// console.log("For loop bro");
+
+// breaking a loop - use break
+// skipping the iteration - use continue
+// important in if conditions using ?, continue, break are not allowed
+// labels - used to name a loop to break or continue outer loops
+// outer: for (let i = 0; i < 3; i++) {
+//   console.log(`i = ${i}`);
+//   inner: for (let j = 0; j < 3; j++) {
+//     if (j === 2) continue outer;
+//     console.log(`   j = ${j}`);
+//   }
+// }
+// console.log("Loop ended");
+
+// Exercises
+// let i = 0;
+// while (i++ < 5) console.log(i);
+// for (let i = 0; i < 5; ++i) console.log(i);
+
+// output even numbers from 2 to 10
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 !== 0) continue;
+//   console.log(i);
+// }
+// for (let i = 0; i < 3; i++) {
+//   alert(`number ${i}!`);
+// }
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number ${i}!`);
+//   i++;
+// }
+
+// prime numbers
+// for (let i = 2; i <= 50; i++) {
+//   let isPrime = true;
+
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       isPrime = false;
+//       break; // It's divisible by another number, so not prime
+//     }
+//   }
+
+//   if (isPrime) {
+//     console.log(i); // Outputs: 2, 3, 5, 7
+//   }
+// }
