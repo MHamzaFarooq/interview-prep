a650bf8f001ca8263c9097ff72ad3884b063d01c
Hamza <hamzafarooq49ml@gmail.com>
2026-02-14 15:10:44 +0500

    array intersection leetcode

--- recursion.js
+++ recursion.js
@@ -1,10 +1,27 @@
 // Fibonacci sequence using recursion
 
-function fib(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
+// function fib(n) {
+//   if (n <= 1) {
+//     return n;
+//   } else {
+//     return fib(n - 1) + fib(n - 2);
+//   }
+// }
+// console.log(fib(4));
+
+// Intersection of two arrays
+
+function intersection(nums1, nums2) {
+  let temp1 = [...new Set([...nums1])];
+  let temp2 = [...new Set([...nums2])];
+  let result = [];
+
+  for (let item of temp1) {
+    if (temp2.includes(item)) {
+      result.push(item);
+    }
   }
+  return result;
 }
-console.log(fib(4));
+
+console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
