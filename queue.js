8caf5e99a4784df6e54f2b10dacbe0936fb518f3
Hamza <hamzafarooq49ml@gmail.com>
2026-02-18 16:53:04 +0500

    implemented queue

---
+++ queue.js
@@ -0,0 +1,25 @@
+class Queue {
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+  enqueue(item) {
+    this.items[this.count] = item;
+    this.count++;
+    console.log(`${item} added into the queue at position ${this.count - 1}`);
+  }
+  dequeue() {
+    const item = this.items.shift();
+    console.log(`${item} removed from the queue at position`);
+    this.count--;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
