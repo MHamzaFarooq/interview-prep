daead53c9ff79ba8da1e6a6943677ffdacaac9e0
Hamza <hamzafarooq49ml@gmail.com>
2026-02-18 17:55:50 +0500

    solved valid parantheses

--- stack.js
+++ stack.js
@@ -1,48 +1,68 @@
-// we can only remove from top of the stack
+// // we can only remove from top of the stack
 
-// stack implamantation
+// // stack implamantation
 
-class Stack {
-  constructor() {
-    this.items = [];
-    this.count = 0;
-  }
-  push(element) {
-    this.items[this.count] = element;
-    console.log(`${element} added to ${this.count}`);
-    this.count++;
-    // return this.count - 1;
-  }
+// class Stack {
+//   constructor() {
+//     this.items = [];
+//     this.count = 0;
+//   }
+//   push(element) {
+//     this.items[this.count] = element;
+//     console.log(`${element} added to ${this.count}`);
+//     this.count++;
+//     // return this.count - 1;
+//   }
 
-  pop() {
-    let deleteItem = this.items[this.count - 1];
-    this.count--;
-    console.log(`${deleteItem} removed from stack`);
-    return deleteItem;
-  }
+//   pop() {
+//     let deleteItem = this.items[this.count - 1];
+//     this.count--;
+//     console.log(`${deleteItem} removed from stack`);
+//     return deleteItem;
+//   }
 
-  // see what is on top of the stack
-  peak() {
-    let topItem = this.items[this.count - 1];
-    console.log(`${topItem} is at the top of stack`);
-    return topItem;
-  }
+//   // see what is on top of the stack
+//   peak() {
+//     let topItem = this.items[this.count - 1];
+//     console.log(`${topItem} is at the top of stack`);
+//     return topItem;
+//   }
 
-  // check if stack is empty
-  isEmpty() {
-    console.log(this.count === 0 ? "stack is empty" : "stack is not empty");
-  }
-}
+//   // check if stack is empty
+//   isEmpty() {
+//     console.log(this.count === 0 ? "stack is empty" : "stack is not empty");
+//   }
+// }
 
-const stack = new Stack();
+// const stack = new Stack();
 
-stack.peak();
-stack.push(10);
-stack.push(20);
-stack.push(30);
-stack.pop();
-stack.pop();
-stack.pop();
-stack.isEmpty();
+// stack.peak();
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+// stack.pop();
+// stack.pop();
+// stack.pop();
+// stack.isEmpty();
 
 // pretty easy ehh
+
+function isValid(s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else {
+      const top = stack.pop();
+      if (
+        (s[i] === ")" && top !== "(") ||
+        (s[i] === "}" && top !== "{") ||
+        (s[i] === "]" && top !== "[")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+console.log(isValid("([)]"));
